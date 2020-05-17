78a62ecd72cb77ac516f6fa4bd9b75c897887761
Vu_T-Solution <leanhvu86@gmail.com>
2020-05-17 10:27:08 +0700

    update file test

---
+++ insert.sql
@@ -0,0 +1,102 @@
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Gustavo','Ponce','test@test.com','1313213213',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Bob','Marley','one@love.com','6483748590',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('David','Gilmour','high@hopes.com','7648909831',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Joh','Lenno','standby@me.com','7689485620',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Ozzy','Osbourne','children@grave.com','6483748590',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Jimmy','Page','stairway@heaven.com','7648909831',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Jimi','Hendrix','purple@haze.com','8754091236',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Sex','Pistols','save@queen.com','6729098761',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Jim','Morriso','riders@storm.com','8754091236',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Richard','Blackmore','highway@star.com','8754091236',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Jay','Kay','cosmic@girl.com','0926389871',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('David','Bowie','heroes@oneday.com','4338490981',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Bob','Dyla','knocking@doors.com','4338490981',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Manu','Chao','mala@vida.com','8923098753',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('The','Specials','ghost@thown.com','7590498573',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Jymmy','Cliff','see@clearly.com','4338490981',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('The','Temptations','my@girl.com','7639864096',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Simo','Garfunkel','mr@robinson.com','8750987531',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('catch','22','takes@sometime.com','7098653427',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Janis','Jopli','cry@baby.com','6739087641',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Lou','Red','wild@side.com','6789045678',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Iggy','Pop','the@passenger.com','6934980751',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('Dead','Kennedys','holiday@cambodia.com','2389096457',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('The','Cure','dont@cry.com','8749340987',1);
+INSERT INTO`qlduan`.`employee`(`name`,`last_name`,`email`,`phone`,`active`)VALUES('5','PRATIK','19','fdsf',1);
+ 
+ ALTER TABLE station AUTO_INCREMENT = 1;
+
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Ga Hà Nội','128 LêDuẩn-HoànKiếm','1000','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Hồ Gươm','75 Đinh Tiên Hoàng-HoànKiếm','1000','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Công viên LêNi','30ĐiệnBiênPhủ-BaĐÌnh','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Bến xe Kim Mã','1KimMã-BaĐình','1000','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Khách sạn Deawoo','308KimMã-BaĐình','1000','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Khách sạn Cầu Giấy','120CầuGiấy-CầuGiấy','1000','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Đại học sư phạm Hà Nội','150XuânThủy-CầuGiấy','1000','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Nghĩa trang Mai Dịch','NgãtưHồTùngMậu-TrầnVỹ-BắcTừLiêm','1000','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Văn Tiến Dũng','NgãtưVănTiếnDũng-CầuDiễn-BắcTừLiêm','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Đại học CN Nhổ','300CầuDiển-BắcTừLiêm','1000','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Cát Linh','1HoàngCầumới-ĐốngĐa','1000','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Hoàng Cầu','20HoàngCầu-ĐốngĐa','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Thái Thịnh','NgãtưTháiThịnh-YênLãng-ĐốngĐa','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Chính Kinh','200NguyễnTrãi-ThanhXuâ','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Đại học Hà Nội','398NguyễnTrãi-ThanhXuâ','500','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Hồ Gươm Plaza','NgãtưNguyễnKhuyến-TrầnPhú-HàĐông','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Bưu điện Hà Đông','2QuangTrung-HàĐông','500','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Khu đô thị Văn Phú','NgãtưVănPhú-QuangTrung-HàĐông','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Bến xe Yên Nghĩa','BaLa-BôngĐỏ-HàĐông','1000','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Trần Hưng Đạo','NgãtưTrầnHưngĐạo-HàngBài-HoànKiếm','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Trần Quang Khải','2TrầnKhánhDư-HoànKiếm','1000','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Bến xe Long Biê','CầuLongBiên-BaĐình','1000','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Bến xe Gia Lâm','61NguyễnSơn-LongBiê','1000','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Ngô Gia Tự','2NgôGiaTự-LongBiê','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Cầu Đuống','654NgôGiaTự-LongBiê','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Thị trấn Yên Viê','ThịtrấnYênViên-YênViê','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Ninh Hiệp','NgãbaNinhHiệp-Quốclộ1-YênViê','500','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Bộ Công An Phạm Văn Đồng','57PhạmVănĐồng-BắcTừLiêm','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Khu đô thị Ciputra-PVD','NgãbaPhạmVănĐồng-NguyễnHoàngTôn-BắcTừLiêm','500','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Mê Linh Plaza','ĐốidiệnkhuCNQuangMinh-ĐôngAnh','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Sân bay Nội Bài','PhúMinh-SócSơ','1000','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Khu đô thị Sa La','NgãbaSaLa-đường70-HàĐông','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Tả Thanh Oai','NgãbaTảThanhOai-Đường70-ThanhTrì','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Nghĩa trang Văn Điể','Đường70-ThanhTrì','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Thị trấn Văn Điể','km30Quốclộ1-ThanhTrì','500','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Bệnh viện nội tiết TW','TứHiệp-ThanhTrì','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Bến xe Nước Ngầm','1NgọcHồi-ThanhTrì','1000','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Bệnh viện Bạch Mai','NgãtưVọng-HaiBàTrưng','500','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Ga Giáp Bát','500GiảiPhóng-HaiBàTrưng','1000','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Khu đô thị Gamuda','Vànhđai3-HoàngMai','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Chợ đầu mối Lĩnh Nam','NgãbaTânMai-TamTrinh-HoàngMai','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Time city','548MinhKhai-HoàngMai','500','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Nguyễn Khoái','NgãbaNguyễnKhoái-TrầnKhátChân-HaiBàTrưng','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Bưởi','27VõChíCông-TâyHồ','500','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Đường Thanh Niê','1ThanhNiên-TâyHồ','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Trung Yên Plaza','1TrungHòa-CầuGiấy','500','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Atermis-LêTrọngTấ','2LêTrọngTấn-ThanhXuâ','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Khu đô thị Ciputra-Võ Chí Công','NgãtưVõChíCông-NguyễnHoàngTôn-TâyHồ','500','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Trung tâm hội nghị Quốc Gia','1PhạmHùng-NamTừLiêm','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Thiên đường Bảo Sơ','NgãbaLêTrọngTấn-ĐạilộThăngLong-NamTừLiêm','500','5h00','11h00','2');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Làng văn hóa các dân tộc','NgãbaHòaLạc-ThạchThất','500','5h00','11h00','1');
+INSERT INTO`qlduan`.`station`(`name_station`,`address_station`,`person_capacity`,`start_time`,`end_time`,`type_station`)VALUES('Khu công nghệ cao Hòa Lạc','KM40ĐạilộThăngLong-ThạchThất','500','5h00','11h00','2');
+
+
+INSERT INTO `qlduan`.`route_rail` (`name_route`, `lst_station`, `create_date`, `update_date`, `status`) VALUES  ('Tuyến 1', 'Ga Hà Nội - Hồ Gươm - Công viên Lê Nin - Bến xe Kim Mã - Khách sạn Deawoo - Khách sạn Cầu Giấy - Đại học sư pham Hà Nội - Nghĩa trang Mai Dịch - Văn Tiến Dũng - Đại học Nhổm', '13/06/2019', NULL, 1);
+INSERT INTO `qlduan`.`route_rail` (`name_route`, `lst_station`, `create_date`, `update_date`, `status`) VALUES  ( 'Tuyến 2', 'Cát Linh - Hoàng Cầu - Thái Thịnh - Chính Kinh - Đại học Hà Nội - Hồ Gươm Plaza - Bưu điện Hà Đông - Khu đô thị Văn Phú - Bến xe Yên Nghĩa', '13/06/2019', NULL, 1);
+INSERT INTO `qlduan`.`route_rail` (`name_route`, `lst_station`, `create_date`, `update_date`, `status`) VALUES  ( 'Tuyến 3', 'Ga Hà Nội - Trần Hưng Đạo - Trần Quang Khải - Bến xe Long Biên - Bến xe Gia Lâm - Ngô Gia Tự - Cầu Đuống - Thị trấn Yên Viên - Ninh Hiệp', '13/06/2019', NULL, 1);
+INSERT INTO `qlduan`.`route_rail` (`name_route`, `lst_station`, `create_date`, `update_date`, `status`) VALUES  ( 'Tuyến 4', 'Đại học CN Nhổn - Nghĩa trang Mai Dịch - Bộ công an Phạm Văn Đồng - Khu Đô thị Ciputra - Mê Linh Plaza - Sân bay Nội Bài', '13/06/2019', NULL, 1);
+INSERT INTO `qlduan`.`route_rail` (`name_route`, `lst_station`, `create_date`, `update_date`, `status`) VALUES  ( 'Tuyến 5', 'Bến xe Yên Nghĩa - Khu đô thị Văn Phú - Bưu điện Hà Đông - Khu đô thị Sa La - Tả Thanh Oai - Nghĩa trang Văn Điển - Thị trấn Văn Điển - Bệnh viện Nội tiết trung ương - Bến xe nước ngầm', '13/06/2019', NULL, 1);
+INSERT INTO `qlduan`.`route_rail` (`name_route`, `lst_station`, `create_date`, `update_date`, `status`) VALUES  ('Tuyến 6', 'Ga Long Biên - Hồ Gươm - Ga Hà Nội- Bệnh viện Bạch Mai - Ga Giáp Bát - Bến xe Nước Ngầm', '13/06/2019', NULL, 1);
+INSERT INTO `qlduan`.`route_rail` (`name_route`, `lst_station`, `create_date`, `update_date`, `status`) VALUES  ( 'Tuyến 7', 'Bến xe Nước Ngầm - Khu đô thị Gamuda - Chợ đầu mối Lĩnh Nam - Time city - Nguyễn Khoái - Trần Quang Khải - Bến xe Long Biên', '13/06/2019', NULL, 1);
+INSERT INTO `qlduan`.`route_rail` (`name_route`, `lst_station`, `create_date`, `update_date`, `status`) VALUES  ( 'Tuyến 8', 'Cát Linh - Khách sạn Deawoo - Bưởi - Khu đô thị Ciputra - Chợ hoa Quảng Bá - Đường Thanh Niên - Bến xe Long Biê', '13/06/2019', NULL, 1);
+INSERT INTO `qlduan`.`route_rail` (`name_route`, `lst_station`, `create_date`, `update_date`, `status`) VALUES  ( 'Tuyến 9', 'Khu đô thị Ciputra - Bưởi - Khách sạn Cầu Giấy - Trung Yên Plaza - Chính Kinh - Tòa nhà Atermis - Bệnh viện Bạch Mai - Time city - Trần Quang Khải', '13/06/2019', NULL, 1);
+INSERT INTO `qlduan`.`route_rail` (`name_route`, `lst_station`, `create_date`, `update_date`, `status`) VALUES  ( 'Tuyến 10', 'Bến xe Kim Mã - Khách sạn Deawoo - Trung Yên plaza - Trung tâm hội nghị Quốc Gia - Thiên đường Bảo Sơn - Khu công nghệ cao Hòa Lạc -Làng văn hóa các dân tộc', '13/06/2019', NULL, 1);
+
+CREATE TABLE IF NOT EXISTS login_user (
+    id INT AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+   password VARCHAR(255) NOT NULL,
+    role VARCHAR(25) NOT NULL,
+     active Int,
+    PRIMARY KEY (id)
+) 
+
